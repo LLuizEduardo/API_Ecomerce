07b4c0e29f9b13035d5e9dc9b616c2b04a8967e9
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-11-23 12:42:27 -0300

    funcoes carrinho

--- Cliente/Controllers/CarrinhoController.cs
+++ Cliente/Controllers/CarrinhoController.cs
@@ -20,6 +20,62 @@ namespace API.Controllers
             _banco = banco;
         }
 
+        [HttpGet]
+        [Route("ver")]
+        public IEnumerable<Carrinho> Get()
+        {
+            return _banco.Carrinho.ToList();
+        }
 
+        [HttpPost]
+        [Route("adicionar")]
+        public async Task<Carrinho> Post(Produto produto,
+                                         int quantidade)
+        {
+            try
+            {
+                var carrinho = new Carrinho
+                {
+                    DataAdicao = DateTime.Now,
+                    Produto = produto,
+                    Quantidade = quantidade
+                };
+
+                _banco.Add(carrinho);
+                await _banco.SaveChangesAsync();
+
+                return carrinho;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("editar")]
+        public async Task<IActionResult> Put(int id, [FromBody] Carrinho carrinho)
+        {
+            if (id != carrinho.Id)
+            {
+                return BadRequest();
+            }
+
+            _banco.Update(carrinho);
+            await _banco.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("apagar")]
+        public async Task Delete(int id)
+        {
+            var carrinho = _banco.Carrinho.FirstOrDefault(carrinho => carrinho.Id == id);
+            if (carrinho != null)
+            {
+                _banco.Remove(carrinho);
+                await _banco.SaveChangesAsync();
+            }
+        }
     }
 }
